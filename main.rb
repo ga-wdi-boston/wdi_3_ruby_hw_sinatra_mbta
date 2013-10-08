cd78bc1c98494d36d83a7c053f9bd24b9dda7d4e
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-08 19:06:03 -0400

    Files print info, just need to do CSS

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
-# require_relative './mbta'
 
 get '/trip' do
 	erb :trip_new
@@ -18,7 +17,7 @@ end
 post '/trip/create' do
 	start_info = params[:home].split("-")
 	end_info = params[:destination].split("-")
-binding.pry
+
 	@home = start_info[1]
 	@home_line = start_info[0].to_sym
 	@destination = end_info[1]
@@ -35,30 +34,28 @@ binding.pry
 		end
 
 		def same_line(home_line, home, destination_line, destination)
-			home_index = calc_index(home_line, home)
-			destination_index = calc_index(destination_line, destination)
+			home_index = calc_index(@home_line, @home)
+			destination_index = calc_index(@destination_line, @destination)
 			return (home_index - destination_index).abs
 		end
 
 		def diff_lines(home_line, home, destination_line, destination)
-			home_index = calc_index(home_line, home)
-			destination_index = calc_index(destination_line, destination)
+			home_index = calc_index(@home_line, @home)
+			destination_index = calc_index(@destination_line, @destination)
 
-			home_mid_point = calc_index(home_line, "park_st")
-			destination_mid_point = calc_index(destination_line, "park_st")
+			home_mid_point = calc_index(@home_line, "park_st")
+			destination_mid_point = calc_index(@destination_line, "park_st")
 
 			first_leg = (home_index - home_mid_point).abs.to_i
 			second_leg = (destination_index - destination_mid_point).abs.to_i
 			first_leg + second_leg
 		end
 
-	if home_line == destination_line
-		calc = "You have #{same_line(home_line, home, destination_line, destination)} stops until your destination."
-	else
-		calc = "You have #{diff_lines(home_line, home, destination_line, destination)} stops until your destination."
-	end
-
-
+		if @home_line == @destination_line
+			@calc = same_line(@home_line, @home, @destination_line, @destination)
+		else
+			@calc = diff_lines(@home_line, @home, @destination_line, @destination)
+		end
 
 	erb :trip_show #same as get above
 end
