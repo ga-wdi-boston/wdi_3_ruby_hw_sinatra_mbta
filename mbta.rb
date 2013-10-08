9e0cebdc20ec6e7735addaa4408822fd2cdea8d8
Abby Howell <abby.g.howell@gmail.com>
2013-10-08 18:39:15 -0400

    everything working

--- mbta.rb
+++ mbta.rb
@@ -3,68 +3,35 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
-	@red_stations = %w(Alewife Davis Porter Harvard Central Kendall Park South_Station)
-	@green_stations = %w(Haymarket Government_Center Park Boylston Arlington Copley)
-	@orange_stations = %w(North_Station Haymarket Park State Downtown_Crossing Chinatown Back_Bay Forest_Hills)
+	@heading = "MBTA Trip Planner"
+	@red_stations = %w(Alewife Davis Porter Harvard Central Kendall Park South\ Station)
+	@green_stations = %w(Haymarket Government\ Center Park Boylston Arlington Copley)
+	@orange_stations = %w(North\ Station Haymarket Park State Downtown\ Crossing Chinatown Back\ Bay Forest\ Hills)
 	erb :form
 end
 
-post '/create' 
+post '/create' do
 	
-	binding.pry
+	@heading = "MBTA Trip Planner Results"
 
-	@red_stations = %w(Alewife Davis Porter Harvard Central Kendall Park South_Station)
-	@green_stations = %w(Haymarket Government_Center Park Boylston Arlington Copley)
-	@orange_stations = %w(North_Station Haymarket Park State Downtown_Crossing Chinatown Back_Bay Forest_Hills)
+	@red_stations = %w(Alewife Davis Porter Harvard Central Kendall Park South\ Station)
+	@green_stations = %w(Haymarket Government\ Center Park Boylston Arlington Copley)
+	@orange_stations = %w(North\ Station Haymarket Park State Downtown\ Crossing Chinatown Back\ Bay Forest\ Hills)
 
-	@start_station = params[:start].scan(/: (\w+)/)
-	@end_station = params[:end].scan(/: (\w+)/)
+	@subway_lines = {"Red" => @red_stations, "Green" => @green_stations, "Orange" => @orange_stations}
+
+	@start_station = params[:start].scan(/: (\w+ *\w*)/).flatten[0]
+	@end_station = params[:end].scan(/: (\w+ *\w*)/).flatten[0]
 	@start_line = params[:start].scan(/\w+/)[0]
 	@end_line = params[:end].scan(/\w+/)[0]
 
 	if @start_line == @end_line
-		case @start_line
-		when "Red"
-			a = @red_stations.index(@start_station)
-			b = @red_stations.index(@end_station)
-			@trip_distance = (a - b).abs
-		when "Green"
-			a = @green_stations.index(@start_station)
-			b = @green_stations.index(@end_station)
-			@trip_distance = (a - b).abs
-		when "Orange"
-			a = @orange_stations.index(@start_station)
-			b = @orange_stations.index(@end_station)
-			@trip_distance = (a - b).abs
-		else
-			"Error"
-		end
+		a = @subway_lines[@start_line].index(@start_station)
+		b = @subway_lines[@end_line].index(@end_station)
+		@trip_distance = (a - b).abs
 	else
-		
-		case @start_line
-
-		when "Red"
-			a = (@red_stations.index(@start_station) - @red_stations.index("Park")).abs
-		when "Green"
-			a = (@green_stations.index(@start_station) - @green_stations.index("Park")).abs
-		when "Orange"
-			a = (@orange_stations.index(@start_station) - @orange_stations.index("Park")).abs
-		else
-			a = 100
-		end
-
-		case @end_line
-
-		when "Red"
-			b = (@red_stations.index(@end_station) - @red_stations.index("Park")).abs
-		when "Green"
-			b = (@green_stations.index(@end_station) - @green_stations.index("Park")).abs
-		when "Orange"
-			b = (@orange_stations.index(@end_station) - @orange_stations.index("Park")).abs
-		else
-			b = 900
-		end
-
+		a = (@subway_lines[@start_line].index(@start_station) - @subway_lines[@start_line].index("Park")).abs
+		b = (@subway_lines[@end_line].index(@end_station) - @subway_lines[@end_line].index("Park")).abs
 		@trip_distance = a + b
 	end
   erb :mbta
