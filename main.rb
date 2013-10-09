48ee01aa28a74ae1691d787a4ff7e81f20d38b77
Rahul Horé <hore.rahul@gmail.com>
2013-10-09 07:14:55 -0400

    Creates a basic dropdown menu

--- main.rb
+++ main.rb
@@ -1,3 +1,12 @@
 require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require_relative 'mbta'
+
+get '/mbta/create' do
+  erb :mbta_form
+end
+
+post '/mbta/new' do
+  erb :mbta
+end
