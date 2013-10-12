fe7ea5111201f7ab315ade8aab667369eef56e50
Rahul Horé <hore.rahul@gmail.com>
2013-10-11 23:16:04 -0400

    Fixes an odd rendering issue in Safari

--- mbta.rb
+++ mbta.rb
@@ -1,8 +1,8 @@
-RED_LINE =    ['south station', 'park st', 'kendall', 'central',
+RED_LINE =    ['south_station', 'park_st', 'kendall', 'central',
                'harvard', 'porter', 'davis', 'alewife']
-GREEN_LINE =  ['haymarket', 'government center', 'park st', 'boylston', 'arlington', 'copley']
-ORANGE_LINE = ['north station', 'haymarket', 'park st', 'state',
-               'downtown crossing', 'chinatown', 'back bay', 'forest hills']
+GREEN_LINE =  ['haymarket', 'government_center', 'park_st', 'boylston', 'arlington', 'copley']
+ORANGE_LINE = ['north_station', 'haymarket', 'park_st', 'state',
+               'downtown_crossing', 'chinatown', 'back_bay', 'forest_hills']
 
 MBTA_SUBWAY_SYSTEM = { red: RED_LINE, green: GREEN_LINE, orange: ORANGE_LINE }
 
@@ -18,6 +18,6 @@ def calculate_number_of_stops(line_on, point_of_departure, line_off, destination
   if line_on == line_off
     return calculate_distance_between_stops(line_on, point_of_departure, line_off, destination)
   else
-    return calculate_distance_between_stops(line_on,  point_of_departure, line_on, 'park st') +  calculate_distance_between_stops(line_off, 'park st', line_off, destination)
+    return calculate_distance_between_stops(line_on,  point_of_departure, line_on, 'park_st') +  calculate_distance_between_stops(line_off, 'park_st', line_off, destination)
   end
 end
